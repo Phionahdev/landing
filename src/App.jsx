4072794155fafe2754850ad862294b486a889259
Phionahdev <mutoniphionah10@gmail.com>
2023-10-11 01:26:31 -0700

    my first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/taskpage" element={<Taskpage />} />
+        <Route index element={<Taskpage />} />
       </Routes>
     </>
   );
